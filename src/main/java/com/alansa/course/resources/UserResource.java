d1d1b56133cc3edc5f8af740763fa8450068897a
AlanAssuncao <alansoaresassuncao@gmail.com>
2022-05-02 21:31:59 -0300

    Classe user e resource

---
+++ src/main/java/com/alansa/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.alansa.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alansa.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Alan", "alan@gmail.com", "11123456", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+	
+}
