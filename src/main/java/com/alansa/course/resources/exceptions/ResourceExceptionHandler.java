f515f9a752e0f2d5e9317aa6346455ef3907d10d
AlanAssuncao <alansoaresassuncao@gmail.com>
2022-05-11 21:46:52 -0300

    Exception handling - delete

--- src/main/java/com/alansa/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/alansa/course/resources/exceptions/ResourceExceptionHandler.java
@@ -22,4 +22,11 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(status).body(err);
 	}
 	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+		String error = "Erro de banco de dados";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
 }
